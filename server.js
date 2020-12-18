05f8292b58c73a3e725cc70727433b8784257a16
Jesus Moris <jesus@soluciontotal.cl>
2020-12-18 05:02:03 -0300

    fixy

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const WebSocket = require('ws');
+const moment = require('moment');
 
 const webSocketServer = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
 webSocketServer.on('connection', webSocket => {
   webSocket.on('message', message => {
     var dt = JSON.parse(message);
+    dt.date = moment().format('lll');
     console.log('Received:', dt);
-    broadcast(message);
+    broadcast(JSON.stringify(dt));
   });
 });
 
